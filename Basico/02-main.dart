a20cd9646f6eb264eca1682c5b4509e6c102fd86
Arllan Pablo <arllanpablo@gmail.com>
2020-05-07 17:08:50 -0300

    adicionado pasta basicos sobre dart

---
+++ Basico/02-main.dart
@@ -0,0 +1,4 @@
+main() {
+    // corpo principal onde vamos codificar
+    print("aprender fundamentos básicos!");
+}
---
+++ Basico/03-variaveis.dart
@@ -0,0 +1,14 @@
+main() {
+  String nome = "arllan pablo"; // variavel de tipo literal
+  int idade = 15; // variavel do tipo inteiro
+  double idade2 = 25.0 ; // variavel do tipo numero flutuante
+  bool status = true ; // variavel do tipo logico 
+
+  print("imprimir um texto"); // imprimir texto
+  print(25);//imprimir numeros
+  print(3.12);// imprimir numero flutuante
+  print("meu nome é:" + nome); // concatenar
+  print(idade2);
+   print(idade);
+  print(status);
+}
